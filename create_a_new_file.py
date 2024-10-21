83220119ccb0a73401c4db2324c71b847ee947ef
Dewy_Rite <dwaynewright1@outlook.com>
2024-10-21 10:49:13 -0500

    Update to Create_a_new_file

--- create_a_new_file.py
+++ create_a_new_file.py
@@ -1,9 +1,14 @@
-file_name = input(" Enter File Name Here : ")
+print("Please select an option below\n\n  1. Create New File\n\n  2. Append Existing File")
+
+# Get the file name from the user
+file_name = input("Enter File Name Here : ")
+file_content: str = input("Please enter file contents here: ")
 
 with open(file_name, 'a') as file:
-    file.write("Now the file has more content!")
+    file.write('\n')
+    file.write(file_content)
     file.close()
 
 #open and read the file after the appending:
-f = open("demofile2.txt", "r")
-print(f.read())
+file = open(file_name, "r")
+print(file.read())
