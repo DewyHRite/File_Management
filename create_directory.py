b0e51d33011ed842a63859998babf6fcd470a31c
Kaciii <115907575+KynxgKacii@users.noreply.github.com>
2024-10-23 14:58:41 -0500

    create_directory.py

---
+++ create_directory.py
@@ -0,0 +1,13 @@
+import os
+
+# Create a directory
+dir_name = input ("\nEnter name of directory: ")
+
+try:
+  # Creating the directory directing in the OS
+    os.mkdir(dir_name)
+    print(f"Directory: '{dir_name}' created.")
+except DirExistsError:
+    print(f"Directory: '{dir_name}' already exists.")
+except Exception as e:
+    print(f"Error creating directory: {e}")
