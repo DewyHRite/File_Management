32ff5b0288433257fd99764fc438b196ee9c9a17
Kaciii <115907575+KynxgKacii@users.noreply.github.com>
2024-11-03 18:27:04 -0500

    Update Rename_a_file.py

--- Rename_a_file.py
+++ Rename_a_file.py
@@ -1,15 +1,16 @@
 import os
 
 # Rename a file
-new_name = input ("\nEnter new name for the file along with extension: ")
+new_filename = input ("\nEnter new name for the file along with extension: ")
 
 try:
 # Renaming file directly in the OS
-    os.rename(file_name, new_name)
-    print(f"File renamed from {file_name} to {new_name}.")
+    os.rename(file_name, new_file)
+    print(f"File renamed from {file_name} to {new_file}.")
 
 # Updating file_name variable to reflect new file name
-    file_name = new_name
+    file_name = new_filename
+    
 except FileNotFoundError:
     print(f"The file {file_name} does not exist.")
 except Exception as e:
